1265a7c53f98aff71fdff88179a235f69eccc564
legnaa98 <monale98@gmail.com>
2021-02-25 11:37:55 -0500

    testing data frame cleaning script

---
+++ postprocessing.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+csv_path = './' # only for testing, change later to ./csv_files
+
+# open each file and select the description column
+csv_files = os.listdir(csv_path)
+# list only .csv files
+csv_files = [f for f in csv_files if '.csv' in f]
+
+# trim undesired text
+for f in csv_files:
+    df = pd.read_csv(f)
+    description = df['description']
+    description = description.apply(lambda x: x.split('Unidades por Caja')[0])
+    df['description'] = description
+    save_path = os.path.join(csv_path, f)
+    pd.to_csv()
+
+
+
+# replace original column with clean column
